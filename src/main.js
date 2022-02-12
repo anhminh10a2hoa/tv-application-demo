97c32ecef565622fec9f899580a864894d227c5a
Minh Hoang <anhminh10a2hoa@gmail.com>
2022-02-13 00:16:56 +0200

    basic setup

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import '@assets/js/spatial_navigation.js'
 
 createApp(App).mount('#app')
---
+++ vue.config.js
@@ -0,0 +1,14 @@
+const path = require('path');  
+module.exports = {  
+  configureWebpack: {  
+    resolve: {  
+      alias: {  
+        '@': path.resolve(__dirname, '.'),
+        '@src': path.resolve(__dirname, 'src'),
+        '@assets': path.resolve(__dirname, 'src/assets'),
+        '@state': path.resolve(__dirname, 'src/state'),
+        '@components': path.resolve(__dirname, 'src/components'),
+      },  
+    },  
+  }
+}
