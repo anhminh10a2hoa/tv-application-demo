a1b7d9c5206bf99e16accb080471ff84389900b6
Minh Hoang <anhminh10a2hoa@gmail.com>
2022-02-13 19:24:51 +0200

    added scss for the project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <!-- <VideoPlayer
+      :license-server="licenseServer"
+      :manifest-url="manifestUrl"
+      :poster-url="posterUrl"
+    /> -->
+    <TopMenu />
+  </div>
 </template>
 
 <script>
-import HelloWorld from '@components/HelloWorld.vue'
-
+// import VideoPlayer from '@components/player/VideoPlayer.vue';
+import TopMenu from '@components/topmenu/TopMenu.vue';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { TopMenu },
+  // data() {
+  //   return {
+  //     licenseServer: 'https://widevine-proxy.appspot.com/proxy',
+  //     manifestUrl:
+  //       'https://dash.akamaized.net/dash264/TestCases/1c/qualcomm/2/MultiRate.mpd',
+  //     posterUrl:
+  //       'https://upload.wikimedia.org/wikipedia/commons/a/a7/Big_Buck_Bunny_thumbnail_vlc.png'
+  //   };
+  // }
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+@import '@design';
+html,
+body {
+  width: $size-app-width;
+  height: $size-app-height;
+}
+
+body {
+  padding: 0;
+  margin: 0;
+  overflow: hidden;
+  font-family: Tiresias, TiresiasScreenfont, sans-serif;
+  font-size: 18px;
+  color: #000;
+  background: $color-background;
+  background-image: none !important;
+}
+.container {
+  max-width: 1280px;
+  max-height: 720px;
+  height: 720px;
+  width: 1280px;
+  position: absolute;
+  top: 0;
+  left: 0;
 }
 </style>
---
+++ src/components/topmenu/TopMenu.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="topmenu">
+    <img src="@assets/img/sofia-digital-logo.png" alt="" />
+    <div class="topmenu__menu">
+      <a href="#">Home</a>
+      <a href="#">About</a>
+      <a href="#">Contact</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopMenu',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+.topmenu {
+  img {
+    width: 200px;
+    margin-top: 20px;
+    margin-left: 20px;
+  }
+}
+</style>
